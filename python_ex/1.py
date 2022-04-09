d8d55bf95b7dcc3f385efdcada185a749c871e45
HyeWon33 <hjw141@naver.com>
2022-04-10 01:24:04 +0900

    1, 2

--- python_ex/1.py
+++ python_ex/1.py
@@ -34,7 +34,7 @@
 
 
 
-# 006
+## 006
 
 # print("오늘은", "일요일")
 # # >> 오늘은 일요일
@@ -43,13 +43,37 @@
 
 
 
-# 007
-#naver;kakao;sk;samsung
+## 007
+# naver;kakao;sk;samsung
 
 # print("naver;kakao;sk;samsung")
 
-#정답
-print("naver", "kakao", "sk", "samsung", sep=";")
-#sep인자로 ";"를 입력하면 출력되는 값들 사이에 한 칸의 공백대신 세미콜론을 출력한다.
+# 정답
+# print("naver", "kakao", "sk", "samsung", sep=";")
+# sep인자로 ";"를 입력하면 출력되는 값들 사이에 한 칸의 공백대신 세미콜론을 출력한다.
 
 
+
+## 008
+# naver/kakao/sk/samsung
+
+# print("naver", "kakao", "sk", "samsung", sep="/")
+
+
+
+## 009
+# print("first");print("second")
+
+# ...
+
+# 정답
+# print("first", end=""); print("second")
+# # end=""은 print문을 이용해 출력을 완료한 뒤의 내용을 수정할 수 있다.
+# # 기본 값으로는 개행 \n이 들어가 있다.
+
+
+
+# 010
+# 5/3
+
+# print(5/3)
---
+++ python_ex/2.py
@@ -0,0 +1,92 @@
+## 011
+# 삼성전자라는 변수로 50000원을 바인딩. 삼성전자 주식 10주를 보유하고 있을 때 총 평가금액을 출력..
+
+# 바인딩(binding) : a=10에서 "="는 수학에서는 같음을 의미하지만 파이썬에서는 a라는 변수가 10을 가리키게 하라는 의미이다.
+
+# 삼성전자 = 50000
+# 총평가금액 = 삼성전자 * 10
+# print(총평가금액)
+
+
+
+## 012
+# 바인딩
+
+# 시가총액 = 298000000000000
+# 현재가 = 50000
+# PER = 15.79
+# #문제는 바인딩인데 정답은 type출력을 했다.
+# print(시가총액, type(시가총액))
+# print(현재가, type(현재가))
+# print(PER, type(PER))
+
+
+
+## 013
+# 변수 활용 출력
+
+# s = "hello"
+# t = "python"
+
+# print(s+ "!", t)
+
+
+
+## 014
+
+
+# print(2+2*3)
+
+
+
+## 105
+
+# a = "123"
+# print(type(a))
+
+
+
+## 106
+# 문자열 -> 정수형
+
+# num_str = "123"
+# print(num_str, type(num_str))
+# num_int = int(num_str)
+# print(num_int, type(num_int))
+
+
+
+## 017
+# 정수 -> 문자열
+
+# num = 100
+# num_str = str(num)
+# print(num_str, type(num_str))
+
+
+
+## 018
+# 문자열 -> 실수
+
+# str_num = "15.79"
+# print(str_num, type(str_num))
+# num_str = float(str_num)
+# print(num_str, type(num_str))
+
+
+
+## 019 
+
+# year = "2020"
+# int_year = int(year)
+# print(int_year+3)
+# print(int_year+2)
+# print(int_year+1)
+
+
+
+## 020
+
+# mon_price = 48584
+# total_price = mon_price * 36
+# print(total_price)
