2e5b05e643a4b5d9bbf2a662c48f3d934f181e9a
HyeWon33 <hjw141@naver.com>
2022-04-09 23:35:46 +0900

    python 연습

---
+++ python_ex/1.py
@@ -0,0 +1,55 @@
+# 1. 파이썬 시작하기
+
+## 001 
+#화면에 문자열 출력
+
+# print("Hello World")
+
+
+
+## 002
+# '
+
+# print("Mary's cosmetics")
+
+
+## 003
+# "
+
+# print('신씨가 소리질렀다. "도둑이야".')
+
+
+
+## 004
+
+# print("C:\Windows")
+
+
+## 005
+# \t, \n
+
+# print("안녕하세요. \n만나서\t\t반갑습니다.")
+# \n : 개행
+# \t : 탭
+
+
+
+# 006
+
+# print("오늘은", "일요일")
+# # >> 오늘은 일요일
+# print("오늘은"+"일요일")
+# # >> 오늘은일요일
+
+
+
+# 007
+#naver;kakao;sk;samsung
+
+# print("naver;kakao;sk;samsung")
+
+#정답
+print("naver", "kakao", "sk", "samsung", sep=";")
+#sep인자로 ";"를 입력하면 출력되는 값들 사이에 한 칸의 공백대신 세미콜론을 출력한다.
+
+
